71eda1c37ca9792c6d8a967f8eb8b8483bc86059
Thai <ngpthaiphuc@gmail.com>
2022-10-30 18:44:20 -0400

    5 working learning units

--- Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
+++ Assets/Learning Units/Unit 05/Scripts/ChangeTextValue.cs
@@ -9,16 +9,17 @@ public class ChangeTextValue : MonoBehaviour
     // the screen. You will need to add some stuff to this script to make it work...
 
     TextMeshProUGUI textMeshProUGUI;
+    [SerializeField] GameObject cube;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMeshProUGUI = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        textMeshProUGUI.text = "Y coordinate = " + cube.transform.position.y;
     }
 }
